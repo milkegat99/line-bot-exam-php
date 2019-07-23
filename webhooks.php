786e964efad088a9f8a5add742904342816c99dd
milkegat99 <48663469+milkegat99@users.noreply.github.com>
2019-07-23 17:25:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '09MxCV4dElHkhDH+UygLCPT1aMsNT3g//Zyoj986C1BGPjC2ynmkVOTHVNSEGwkrFHP7YvUony1Ij0JS6TeAJ85R6ojfbPLUenUBlyYTqOhPcg1JaCVKP2YwkzYNtqPuI4QI6+ReeGeAP944QH7oPwdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
